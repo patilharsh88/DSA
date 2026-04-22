59a14ca17ba8bef1e9e9cdcc13e02450a0270668
patilharsh88 <patilharshvardhank27502@gmail.com>
2026-04-22 13:48:17 +0530

    Update DiamondElement.java

--- Arrays/DiamondElement.java
+++ Arrays/DiamondElement.java
@@ -1,4 +1,13 @@
 class DiamondElement{
-
+    public static long countEvenSumPairs(int n) {
+        long even = n / 2;        
+        long odd = n - even;      
+        
+        return (even * (even + 1)) / 2 + (odd * (odd + 1)) / 2;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(countEvenSumPairs(n)); 
+    }
   
 }
